a68180ccafa6ea949374475f9e26a96fa50ddc74
Joaquin Martinez <54916596+Joaquinuriel@users.noreply.github.com>
2022-08-29 20:29:14 -0300

    joa-revision

--- actividad-1/joa.py
+++ actividad-1/joa.py
@@ -1,23 +1,33 @@
 # Encontrar todos los primos circulares del 1 al 100
 
 def esPrimo(n):
+    # por cada divisor de n, 
+    # si n es divisible por ese divisor, 
+    # n no es primo
     for i in range(2, n):
         if n % i == 0:
             return False
+    # si n no es divisible por ninguno de los divisores anteriores,
+    # entonces n es primo
     return True
 
-
 def permutar(n):
-    return int(str(n)[::-1])
-
+    # convertir N a una cadena de texto
+    n = str(n)
+    # invertir la cadena de texto
+    n = n[::-1]
+    # devolver N como un entero 
+    return int(n)
 
 def esPrimoCircular(n):
+    # m es el inverso a N
     m = permutar(n)
     return esPrimo(n) and esPrimo(m)
 
-
 lista = [
-    n for n in range(1, 101) if esPrimoCircular(n)
+    # cada numero en el rango 1 al 99 si es primo circular
+    n for n in range(1, 100) if esPrimoCircular(n)
 ]
 
+# imprimir los primos circulares
 print(*lista)
