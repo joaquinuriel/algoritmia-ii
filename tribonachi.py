704447f756fe5302bac8b8d4cb8b68f057a18be8
Joaquin Martínez <54916596+joaquinuriel@users.noreply.github.com>
2022-08-30 21:06:03 -0300

    Update tribonachi.py

--- tribonachi.py
+++ tribonachi.py
@@ -1,5 +1,7 @@
 # calcular tribonachi al 1000
-# guardar los numeros no divisibles por los anteriores y no pares
+# guardar 100 numeros no divisibles por los anteriores y no pares
 
-
+lista = [
+  n + lista[-1] + lista[-2] for n in range(1000) if n < 1000
+]
 
